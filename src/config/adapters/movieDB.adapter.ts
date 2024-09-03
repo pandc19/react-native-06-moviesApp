ed6ecb7a679611fde542af6da880d02d1cfc80fd
Jonathan Pillajo <pandc19@outlook.com>
2024-09-03 07:57:53 -0500

    end section 11

--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,9 +1,11 @@
+import {THE_MOVIE_DB_KEY} from '@env';
 import {AxiosAdapter} from './http/axios.adapter';
 
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '05ef4da9804192bec78c0477c2415a1c',
+    // api_key: '05ef4da9804192bec78c0477c2415a1c',
+    api_key: THE_MOVIE_DB_KEY ?? 'no-key',
     language: 'es',
   },
 });
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {useMovies} from '../../hooks/useMovies';
+import {ScrollView} from 'react-native-gesture-handler';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PosterCarousel} from '../../components/movies/PosterCarousel';
+import {HorizontalCarousel} from '../../components/movies/HorizontalCarousel';
+import {FullScreenLoader} from '../../components/loaders/FullScreenLoader';
 
 export const HomeScreen = () => {
-  const {} = useMovies();
-  
+  const {top} = useSafeAreaInsets();
+
+  const {isLoading, nowPlaying, popular, topRated, upcoming, popularNextPage} =
+    useMovies();
+
+  if (isLoading) {
+    return <FullScreenLoader />;
+  }
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top + 20, paddingBottom: 30}}>
+        {/* Principal */}
+        <PosterCarousel movies={nowPlaying} />
+
+        {/* Populares */}
+        <HorizontalCarousel
+          movies={popular}
+          title="Populares"
+          loadNextPage={popularNextPage}
+        />
+
+        {/* Top Rated */}
+        <HorizontalCarousel movies={topRated} title="Mejor calificadas" />
+
+        {/* Populares */}
+        <HorizontalCarousel movies={upcoming} title="Próximamente" />
+      </View>
+    </ScrollView>
   );
 };
